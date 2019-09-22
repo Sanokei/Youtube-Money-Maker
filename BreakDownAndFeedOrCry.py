ea3b9374d9007b0dbffbe7e6ca03c237290466bf
Sano <36809574+Sanokei@users.noreply.github.com>
2019-09-22 16:10:20 -0400

    Still working on it

--- BreakDownAndFeedOrCry.py
+++ BreakDownAndFeedOrCry.py
@@ -12,21 +12,29 @@ wait for that and stich back together
 
 Name the input files with the 11 code for youtube urls
 '''
+def main():
+  url = ""
+  extractIntoSegments(downloadFile(url))
+def nameFile(url):
+  return url[len(url) - 12: len(url) - 1]
+
 def extractIntoSegments(inputFilename):
-  ffmpeg_extract_subclip(inputFilename, start_time, end_time, targetname="test.mp4")
+  numberOfSegments = getLength(inputFilename)
+  for index in length
+  ffmpeg_extract_subclip(inputFilename, start_time, end_time, targetname= inputFilename + "_" + index)
  
 def stitchTogether(inputFilenameList):
 #ffmpeg -i "concat:input1.mpg|input2.mpg|input3.mpg" -c copy output.mpg
   lilo = "\"concat:"
   for filename in inputFilenameList:
     lilo += filename + ".mp4|"
-  lilo = lilo[0:len(lilo)-1] + \"
+  lilo = lilo[0:len(lilo)-1] + "\""
   
-  output = "DoneProduct"
+  output = nameFile(url) + "_Final"
 
 def downloadFile(url):
     name = YouTube(url).streams.first().download()
-    newname = name.replace(' ','_')
+    newname = nameFile(url) + "_0"
     os.rename(name,newname)
     return newname
 
